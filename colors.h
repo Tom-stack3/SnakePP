d2a6eb8e5307894fd3d8ed066c6a79a880120424
Tommy <Tom-stack3@users.noreply.github.com>
2022-12-21 21:43:25 +0200

    Create colors.h

---
+++ colors.h
@@ -0,0 +1,20 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define C_DARK_RED "\x1b[31m"
+#define C_DARK_GREEN "\x1b[32m"
+#define C_DARK_YELLOW "\x1b[33m"
+#define C_DARK_BLUE "\x1b[34m"
+#define C_DARK_MAGENTA "\x1b[35m"
+#define C_DARK_CYAN "\x1b[36m"
+#define C_DARK_WHITE "\x1b[37m"
+#define C_BRIGHT_RED "\x1b[91m"
+#define C_BRIGHT_GREEN "\x1b[92m"
+#define C_BRIGHT_YELLOW "\x1b[93m"
+#define C_BRIGHT_BLUE "\x1b[94m"
+#define C_BRIGHT_MAGENTA "\x1b[95m"
+#define C_BRIGHT_CYAN "\x1b[96m"
+#define C_BRIGHT_WHITE "\x1b[97m"
+#define C_DEFAULT "\x1b[0m"
+
+#endif
